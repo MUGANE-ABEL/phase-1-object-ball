e76cbb8aa95abb49487f1b7f7a62c92b4a4aea38
Mugane Abel <muganeabel@gmail.com>
2024-04-24 13:19:33 +0300

    Lab

---
+++ src/00-objectball.js
@@ -0,0 +1,142 @@
+function gameObject(){
+    home: {
+        teamName : "Brooklyn Nets"
+        colors : ["Black", "White"]
+        players : {
+            PlayerName: {
+                Info : "Alan Anderson"
+                Info : "Reggie Evans"
+                Info : "Brook Lopez"
+                Info : "Mason Plumlee"
+                Info : "Jason Terry"
+            }
+            Number: {
+                Info : 0
+                Info : 30
+                Info : 11
+                Info : 1
+                Info : 31
+            }
+            Shoe: {
+                Info : 16
+                Info : 14
+                Info : 17
+                Info : 19
+                Info :15
+            }
+            Points: {
+                Info : 22
+                Info : 12
+                Info : 17
+                Info : 26
+                Info :19
+            }
+            Rebounds: {
+                Info : 12
+                Info : 12
+                Info : 19
+                Info : 12
+                Info :2
+            }
+            Assists: {
+                Info : 12
+                Info : 12
+                Info : 10
+                Info : 6
+                Info :2
+            }
+            Steals: {
+                Info : 3
+                Info : 12
+                Info : 3
+                Info : 3
+                Info :4
+            }
+            Blocks: {
+                Info : 1
+                Info : 12
+                Info : 1
+                Info : 8
+                Info :11
+            }
+            SlamDunks: {
+                Info : 1
+                Info : 7
+                Info : 15
+                Info : 5
+                Info :1
+            }
+        }
+    }
+    away: {
+         teamName : "Charlotte Hornets"
+        colors : ["Turquoise", "Purple"]
+        players : {
+            PlayerName: {
+                Info : "Jeff Adrien"
+                Info : "Bismak Biyombo"
+                Info : "DeSagna Diop"
+                Info : "Ben Gordon	"
+                Info : "Brendan Haywood"
+            }
+            Number: {
+                Info : 4
+                Info : 0
+                Info : 2
+                Info : 8
+                Info : 33
+            }
+            Shoe: {
+                Info : 18
+                Info : 16
+                Info : 14
+                Info : 15
+                Info :15
+            }
+            Points: {
+                Info : 10
+                Info : 12
+                Info : 24
+                Info : 33
+                Info :6
+            }
+            Rebounds: {
+                Info : 1
+                Info : 4
+                Info : 12
+                Info : 3
+                Info :12
+            }
+            Assists: {
+                Info : 1
+                Info : 7
+                Info : 12
+                Info : 2
+                Info :12
+            }
+            Steals: {
+                Info : 2
+                Info : 7
+                Info : 4
+                Info : 1
+                Info :22
+            }
+            Blocks: {
+                Info : 7
+                Info : 15
+                Info : 5
+                Info : 1
+                Info :5
+            }
+            SlamDunks: {
+                Info : 2
+                Info : 10
+                Info : 5
+                Info : 0
+                Info :12
+            }
+        }
+    }
+}
+
+console.log(gameObject())
